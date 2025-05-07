1bbaaae61c384085e49430949327d71bf56a972c
Daniel Bustamante <daniel.bus.cor@gmail.com>
2025-05-07 19:54:29 -0400

    Movie class created

---
+++ src/com/danielbuscor/desafio3/models/Movie.java
@@ -0,0 +1,11 @@
+package com.danielbuscor.desafio3.models;
+
+public class Movie {
+    private String title;
+    private int episodeId;
+    private String director;
+    private String releaseDate;
+
+
+
+}
